e9b3e3079b426aebb1d93896179466dde890aa33
Maxime Uteza <muteza@c2r6p6.42nice.fr>
2021-10-29 16:02:32 +0200

    fjeofhjkrsgcehjfg

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: muteza <muteza@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/22 09:59:05 by muteza            #+#    #+#             */
-/*   Updated: 2021/10/27 15:19:57 by muteza           ###   ########.fr       */
+/*   Updated: 2021/10/27 18:16:26 by muteza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,21 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	int		i;
 	char	*str;
-	size_t	size;
 
-	size = len;
-	if (ft_strlen(s) <= len)
-		size = ft_strlen(s);
-	str = malloc(size + 1);
+	if (ft_strlen(s + start) < len)
+		len = ft_strlen(s);
+	if (start > strlen(s))
+	{
+		return (ft_strdup(""));
+	}
+	str = malloc(len + 1);
 	if (!str)
 		return (NULL);
 	i = 0;
-	while (start < len && s[start] != '\0' && size != 0)
+	while ((size_t)i < len)
 	{
-		str[i] = s[start];
+		str[i] = s[start + i];
 		i++;
-		start++;
 	}
 	str[i] = '\0';
 	return (str);
